6bd8f58a5c3464c4024997856ec307b9bad1c0b5
Rodolfo de la Trinidad Casas <rdelatrinidad@hotmail.com>
2023-08-18 20:36:25 -0600

    Agregar validaciones en el formulario de agregar tarea.

--- src/main/java/com/example/rudy/taskmanagerdemo/dto/TaskDto.java
+++ src/main/java/com/example/rudy/taskmanagerdemo/dto/TaskDto.java
@@ -1,6 +1,9 @@
 package com.example.rudy.taskmanagerdemo.dto;
 
 import com.example.rudy.taskmanagerdemo.domain.Status;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,8 +16,12 @@ import lombok.NoArgsConstructor;
 @Builder
 public class TaskDto {
     private Long id;
+    @NotBlank(message = "Title can't be empty.")
+    @Size(min = 4, message = "Title must be at least 4 characters long.")
     private String title;
+    @NotBlank(message = "Description can't be empty.")
     private String description;
+    @NotNull(message = "You must select at least one option.")
     private Status status;
     private LocalDate createdOn;
     private LocalDate updatedOn;
