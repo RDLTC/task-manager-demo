d1fff0af05a72b5e2f089096025801a937079bac
Rodolfo de la Trinidad Casas <rdelatrinidad@hotmail.com>
2023-08-18 18:53:34 -0600

    Agregar passwordencoder BCrypt para codificar las contraseñas.

--- src/main/java/com/example/rudy/taskmanagerdemo/service/UserService.java
+++ src/main/java/com/example/rudy/taskmanagerdemo/service/UserService.java
@@ -7,6 +7,7 @@ import com.example.rudy.taskmanagerdemo.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,12 +17,13 @@ public class UserService{
     
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
     
     public void registerUser(UserDto user){
         User newUser = User.builder()
                         .email(user.getEmail())
                         .userName(user.getUserName())
-                        .password(user.getPassword())
+                        .password(passwordEncoder.encode(user.getPassword()))
                         .rol("USER")
                         .build();
         userRepository.save(newUser);
